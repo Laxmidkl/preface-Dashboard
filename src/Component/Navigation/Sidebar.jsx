85fe699f9ddb0e39679e9aab4de8e88f05e47eff
Laxmidkl <dhakalaxmi74@gmail.com>
2026-05-12 15:12:01 +0545

    feat(sidebar): implement reusable dynamic sidebar navigation

--- src/Component/Navigation/Sidebar.jsx
+++ src/Component/Navigation/Sidebar.jsx
@@ -1,56 +1,144 @@
-import React, { useState } from 'react'
-import { IoIosArrowDown, IoIosArrowForward } from 'react-icons/io';
-import { TbSmartHome } from 'react-icons/tb';
+import React, { useState } from "react";
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
+import { MdOutlineContactPhone } from "react-icons/md";
+import { RiTodoLine } from "react-icons/ri";
+import { SlCalender } from "react-icons/sl";
+import { TbSmartHome } from "react-icons/tb";
 
-const Sidebar = () => {
+function Sidebar() {
+  const [openId, setOpenId] = useState(null);
 
-  const [open, setOpen] = useState(false);
+  const sidebarData = [
+    {
+      section: "menu",
+      id: "dashboard",
+      title: "Dashboard",
+      icon: <TbSmartHome size={20} />,
+      children: [{ title: "Sales" }, { title: "Analysis" }],
+    },
 
+    {
+      section: "apps",
+      items: [
+        {
+          title: "Todo List",
+          icon: <RiTodoLine size={20} />,
+        },
+        {
+          title: "Contact",
+          icon: <MdOutlineContactPhone size={20} />,
+        },
+        {
+          title: "Calendar",
+          icon: <SlCalender size={20} />,
+        },
+      ],
+    },
+
+    {
+      section: "pages",
+      id: "auth",
+      title: "Authentication",
+      icon: <TbSmartHome size={20} />,
+      children: [
+        { title: "Login" },
+        { title: "Recover Password" },
+        { title: "Register" },
+        { title: "Lock screen" },
+      ],
+    },
+
+      {
+      id: "error",
+      title: "Error Pages",
+      icon: <TbSmartHome size={20} />,
+      children: [
+        { title: "Error 404" },
+        { title: "Error 503" },
+        { title: "Error 500" },
+        { title: "Offline Page" },
+      ],
+    },
+  ];
 
   return (
-    <div className=' text-white mt-24 lg:mt-5 '>
-      <div className=' flex gap-3  ml-9'>
-
-        <div className=''>
-          logo
-        </div>
-        <div className=' font-bold text-2xl '>
-          Dusty
-        </div>
+    <div className="text-white mt-24 lg:mt-5">
+      {/* Logo */}
+      <div className="hidden lg:flex gap-3 ml-9 ">
+        <div>logo</div>
+        <div className="font-bold text-2xl mb-2">Preface</div>
       </div>
 
-      <div>
-        <div className=' uppercase text-gray-400 text-[12px] mt-8 font-semibold  ml-9'> menu</div>
-        <ul
-        onClick={() => setOpen(!open)}
-        className=' flex gap-3 mt-4 hover:bg-[#1E2145] w-full h-12 items-center mx-3 rounded-xl border-none cursor-pointer'>
-          <li><TbSmartHome className=' text-gray-500 ml-6' size={23} /></li>
-          <li className=' font-semibold'>Dashboard</li>
-          <li
-          > {open ? <IoIosArrowDown size={14} className=' ml-14 mt-1.5' />: <IoIosArrowForward size={14} className=' ml-14 mt-1.5' /> }   </li>
-
-        </ul>
-        <div
-          className={`overflow-hidden transition-all duration-800 ease-in-out ${open
-              ? "max-h-40 opacity-100"
-              : "max-h-0 opacity-0"
-            }`}
+      {/* Map all the section */}
+      {sidebarData.map((block, index) => (
+        <div 
+        key={index}
+        className="text-gray-500 font-semibold"
         >
-          <div className="">
-            <ul>
-              <li className="p-3 hover:bg-gray-200 cursor-pointer">
-                Home
-              </li>
-
-              <li className="p-3 hover:bg-gray-200 cursor-pointer">
-                About
-              </li>
-            </ul>
+        
+          <div className="uppercase text-gray-400 text-[12px] mt-3 font-semibold ml-8">
+            {block.section}
           </div>
+
+          {/* dropdown  (menu + pages) */}
+          {block.children && (
+            <>
+              <ul
+                onClick={() => setOpenId(openId === block.id ? null : block.id)}
+                className="flex gap-3 mt-2 hover:bg-[#1E2145] w-11/12 p-3 items-center ml-3 rounded-xl cursor-pointer text-[14px]"
+              >
+                <li className="text-gray-500 ml-1">{block.icon}</li>
+                <li className="font-semibold hover:text-white">{block.title}</li>
+
+                <li className="ml-auto">
+                  {openId === block.id ? (
+                    <IoIosArrowDown size={14} />
+                  ) : (
+                    <IoIosArrowForward size={14} />
+                  )}
+                </li>
+              </ul>
+
+              {/* dropdown */}
+              <div
+                className={`overflow-hidden transition-all duration-300 ease-in-out ${
+                  openId === block.id
+                    ? "max-h-60 opacity-100"
+                    : "max-h-0 opacity-0"
+                }`}
+              >
+                <ul className="ml-11 mr-6 text-[14px]">
+                  {block.children.map((item, i) => (
+                    <li
+                      key={i}
+                      className="p-2 hover:bg-[#1E2145] hover:text-white rounded-xl ml-3 cursor-pointer"
+                    >
+                      {item.title}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </>
+          )}
+
+          {/* Apps Section */}
+          {block.items && (
+            <ul>
+              {block.items.map((item, i) => (
+                <li
+                  key={i}
+                  className="flex gap-3 mt-2 hover:bg-[#1E2145] hover:text-white w-11/12 p-2 items-center ml-3 mr-3 rounded-xl cursor-pointer text-[14px]"
+                >
+                  <div className="text-gray-500 ml-2">{item.icon}</div>
+                  <div className="font-semibold">{item.title}</div>
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
-      </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,38 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import Sidebar from '../Component/Navigation/Sidebar'
-import Toolbar from '../Component/Navigation/Toolbar'
-import { Outlet } from 'react-router-dom'
-
-
+import React, { useEffect, useState } from "react";
+import Sidebar from "../Component/Navigation/Sidebar";
+import Toolbar from "../Component/Navigation/Toolbar";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-   const [isOpen, setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(true);
 
-     useEffect(() => {
-
+  useEffect(() => {
     const handleResize = () => {
-
       if (window.innerWidth >= 1024) {
-        setIsOpen(true)   // desktop → open
+        setIsOpen(true);
       } else {
-        setIsOpen(false)  // small screen → closed
+        setIsOpen(false);
       }
+    };
 
-    }
+    handleResize();
 
-    handleResize() // run once on load
+    window.addEventListener("resize", handleResize);
 
-    window.addEventListener("resize", handleResize)
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    return () =>
-      window.removeEventListener("resize", handleResize)
-
-  }, [])
-
-  
   return (
-    <div className='flex '>
-      <div className={`
+    <div className="flex ">
+      <div
+        className={`
           fixed lg:top-0 left-0 md:top-20
           h-screen
           bg-[#131133]
@@ -40,27 +33,24 @@ const Layout = () => {
           transition-all duration-300
           overflow-hidden
          
-          ${isOpen ? 'w-64' : 'lg:w-0 w-0'}
-        `}>
-     <Sidebar/>
-      </div>
-     <div
-       className={`
-          flex-1 transition-all duration-300 w-full
-          ${isOpen ? 'md:ml-64' : 'md:ml-0'}
+          ${isOpen ? "w-64" : "lg:w-0 w-0"}
         `}
-     >
-        <Toolbar
-          isOpen={isOpen}
-          setIsOpen={setIsOpen}
-        
-     />
-      <div className='md:mx-10 '>
-      <Outlet/>
+      >
+        <Sidebar />
       </div>
-     </div>
+      <div
+        className={`
+          flex-1 transition-all duration-300 w-full
+          ${isOpen ? "md:ml-64" : "md:ml-0"}
+        `}
+      >
+        <Toolbar isOpen={isOpen} setIsOpen={setIsOpen} />
+        <div className="md:mx-10 ">
+          <Outlet />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
